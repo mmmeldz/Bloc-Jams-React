c3b51c912b7667532a1c3adaf516863668792ece
Melissa Mendez <mmmeldz@gmail.com>
2018-06-12 19:10:15 -0400

    Albums-assignment-completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -33,10 +33,17 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+           {
+            this.state.album.songs.map((song, index) =>
+             <tr key={index}>
+                <td className="song-number">{index+1}</td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
+            </tr>
+          )}
            </tbody>
          </table>
        </section>
-
     );
   }
 }
