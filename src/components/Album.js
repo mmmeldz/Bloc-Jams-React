227aa1740fecdf769e331c329e37858c8c7aad4b
Melissa Mendez <mmmeldz@gmail.com>
2018-06-12 13:49:30 -0400

    corrected Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import albumData from './../data/albums';
 
-class Library extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { albums: albumData };
-  }
-
+class Album extends Component {
   render() {
-    return (
-      <section className = 'library'>
-      {
-        this.state.albums.map( (album, index) =>
-          <Link to={`/album/${album.slug}`} key={index}>
-            <img src={album.albumCover} alt={album.title} />
-            <div>{album.title}</div>
-            <div>{album.artist}</div>
-            <div>{album.songs.length} songs</div>
-          </Link>
-        )
-      }
+    return(
+      <section className="album">
+      {this.props.match.params.slug} Album will go here
       </section>
     );
   }
 }
 
-export default Library;
+export default Album;
